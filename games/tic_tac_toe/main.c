1792afb9c36e653d8030297ee34475940ad23aa5
Clarisse A. Campos <tetearruda17@gmail.com>
2025-09-06 23:17:15 -0300

    Manutencao requerida:
    Otimizar o matrixRand( ) de 3 loops para 1 aninhado. Tambem eh necessario fazer com que o input seja com base na letra, e nao na linha e coluna, para melhorar a vizualizacao. assim como ha de checar se o usuario nao esta colocando a letra em um lugar ja ocupado.

--- games/tic_tac_toe/main.c
+++ games/tic_tac_toe/main.c
@@ -3,13 +3,20 @@
 #define N 3
 
 void matrixRand(char matrix[N][N]){
-    int i,j;
-
-    for(i = 0;i < N; i++){
-        for(j = 0; j < N; j++){
-            matrix[i][j] = '?';
-        }
-    }
+   int j;
+   // primeira linha
+   for(j = 0;j < N;j++){
+        matrix[0][j] = 65 + j;
+   }
+    //segunda linha
+   for(j = 3;j < N+3;j++){
+        matrix[1][j-3] = 65 + j;
+   }
+    //terceira linha
+   for(j = 6;j < N+6;j++){
+        matrix[2][j-6] = 65 + j;
+   }
+        
 }
 
 void setRole(char matrix[N][N], char role){
@@ -20,10 +27,23 @@ void setRole(char matrix[N][N], char role){
     printf("\nSet COLUMN position: "); scanf("%d", &j);
 
     i -= 1; j -= 1; 
-
     matrix[i][j] = role;
+}
 
+int gameOverCheckOut(char matrix[N][N], char role){
+    int i;
+    // checar linhas e colunas
+    for(i = 0;i < N; i++){
+        if((matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2] && matrix[i][2] == role) || (matrix[0][i] == matrix[1][i] && matrix[1][i] == matrix[2][i] && matrix[2][i] == role)){
+            return 0;
+        }
+    }
+    // checar diagonais
+    if((matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2] && matrix[2][2] == role) || (matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0] && matrix[2][0] == role)){
+        return 0;
+    }
 
+    return 1;
 }
 
 void writeMatrix(char matrix[N][N]){
@@ -43,21 +63,26 @@ void writeMatrix(char matrix[N][N]){
 
 int main(){
 
-    int i;
-
     char matrix[N][N];
 
     matrixRand(matrix); writeMatrix(matrix);
 
-    for(i = 0; /*a decidir*/; i++){
-
+    for(;;){
         setRole(matrix,'X');
-
         writeMatrix(matrix);
+
+        if(!gameOverCheckOut(matrix,'X')){
+            printf("\nGAME OVER! X won.");
+            break;
+        }
 
         setRole(matrix,'O');
-
         writeMatrix(matrix);
+
+        if(!gameOverCheckOut(matrix,'O')){
+            printf("\nGAME OVER! O won.");
+            break;
+        }
     }
 
     return 0;
