eb0c704637699764bbb4064970df4f69071b9e88
Clarisse A. Campos <tetearruda17@gmail.com>
2026-02-19 12:25:15 -0300

    Fuck, I dont know even wtf am i doing

--- rustcookbook/hello-rust/target/debug/deps/helloRust-09913537c78d0aaf.d
+++
@@ -1,5 +0,0 @@
-/home/clarisse/Desktop/codeplay/rustcookbook/hello-rust/target/debug/deps/helloRust-09913537c78d0aaf.d: src/main.rs
-
-/home/clarisse/Desktop/codeplay/rustcookbook/hello-rust/target/debug/deps/helloRust-09913537c78d0aaf: src/main.rs
-
-src/main.rs:
--- rustcookbook/hello-rust/target/debug/helloRust.d
+++
@@ -1,1 +0,0 @@
-/home/clarisse/Desktop/codeplay/rustcookbook/hello-rust/target/debug/helloRust: /home/clarisse/Desktop/codeplay/rustcookbook/hello-rust/src/main.rs
