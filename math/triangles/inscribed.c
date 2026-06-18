ba60753f9ddac952d7f91b794bdc421c0ccd1b8e
Clarisse A Campos <tetearruda17@gmail.com>
2026-06-18 13:26:33 -0300

    Adicionando funcoes a inscribed.c

--- math/triangles/inscribed.c
+++ math/triangles/inscribed.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
-// a = a*b*c/4*r
+//  Area do triângulo inscrito em uma circunferência
+// a = (a*b*c)/(4.0*r);
+
+void inputData(double *a ,double *b ,double *c ,double *r){
+    printf("\nSet side a: ");scanf("%lf", a);
+    printf("\nSet side b: ");scanf("%lf", b);
+    printf("\nSet side c: ");scanf("%lf", c);
+    printf("\nSet set radius: ");scanf("%lf", r);
+}
+double inscribedT_area(double a ,double b ,double c ,double r){
+    return (a*b*c)/(4.0*r);
+}
 int main(){
-    float area,a,b,c,r;
+    double area, a,b,c,r;
 
-    printf("\nSet side a: ");scanf("%f", &a);
-    printf("\nSet side b: ");scanf("%f", &b);
-    printf("\nSet side c: ");scanf("%f", &c);
-    printf("\nSet set radius: ");scanf("%f", &r);
+    inputData(&a,&b,&c,&r);
 
-    area = (a*b*c)/(4.0*r);
+    area = inscribedT_area(a,b,c,r);
 
     printf("\nArea: %.2f", area);
+
     return 0;
 }
