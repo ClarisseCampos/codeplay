fd24b5a5b25fd6666cd9eb7090ab4e5c9c06eeb7
Clarisse A Campos <tetearruda17@gmail.com>
2026-06-18 14:32:21 -0300

    add functions to areaTherorem.c and heron.c

--- math/triangles/areaTheorem.c
+++ math/triangles/areaTheorem.c
@@ -2,15 +2,20 @@
 #include <math.h>
 //a = a*b*sin(teta)/2
 //compilar com: gcc areaTheorem.c -o output -lm
+void inputData(double *a, double *b, double *theta){
+    printf("\nSet side a: ");scanf("%lf", a);
+    printf("\nSet side b: ");scanf("%lf", b);
+    printf("\nSet angle between a-b: ");scanf("%lf", theta);
+}
+double areaTheorem(double a, double b, double theta){
+    theta *= acos(-1.0)/180;
+    return a*b*(sin(theta))/2.0;
+}
 int main(){
-    float area,a,b,theta;
-    printf("\nSet side a: ");scanf("%f", &a);
-    printf("\nSet side b: ");scanf("%f", &b);
-    printf("\nSet angle between a-b: ");scanf("%f", &theta);
+    double area,a,b,theta;
+    inputData(&a,&b,&theta);
 
-    theta *= acos(-1.0)/180; // deg -> rad
-
-    area = a*b*(sin(theta))/2.0;
+    area = areaTheorem(a,b,theta);
 
     printf("\nArea: %.2f", area);
     return 0;
--- math/triangles/heron.c
+++ math/triangles/heron.c
@@ -3,14 +3,21 @@
 //semiperimeter:a+b+c/2
 //area: sqrt(p*(p-a)*(p-b)*(p-c))
 // compile with: gcc heron.c -o output -lm
+void inputData(double *a, double *b, double *c){
+    printf("\nSet side a: ");scanf("%lf", a);
+    printf("\nSet side b: ");scanf("%lf", b);
+    printf("\nSet side c: ");scanf("%lf", c);
+}
+double heronT_area(double a, double b, double c){
+    double smp = (a+b+c)/2.0;
+    return sqrt(smp*(smp - a)*(smp - b)*(smp - c));
+}
 int main(){
-    float area, smp, a, b, c;
-    printf("\nSet side a: ");scanf("%f", &a);
-    printf("\nSet side b: ");scanf("%f", &b);
-    printf("\nSet side c: ");scanf("%f", &c);
+    double area, a, b, c;
 
-    smp = (a+b+c)/2.0;
-    area = sqrt(smp*(smp - a)*(smp - b)*(smp - c));
+    inputData(&a,&b,&c);
+
+    area = heronT_area(a,b,c);
 
     printf("\nArea: %.2f", area);
     
