287d729510f3a4ce297c73348677bf4ef8b27fd1
Clarisse A Campos <tetearruda17@gmail.com>
2026-08-19 16:26:11 -0300

    Modulos

---
+++ stack-based/python-nb/modules/fireworks.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+n = int(input("Valor: "))
+
+for i in range(n,0,-1):
+    print(i)
+    sleep(1)
+
+print('BUM! BUM! POOW!')
---
+++ stack-based/python-nb/modules/math.py
@@ -0,0 +1,7 @@
+from math import sqrt,ceil,floor
+
+num = 5
+
+raiz = sqrt(num)
+print('Square root of {} is {:.4f}'.format(num,raiz))
+print('Ceil: {} Floor {}'.format(ceil(raiz),floor(raiz)))
