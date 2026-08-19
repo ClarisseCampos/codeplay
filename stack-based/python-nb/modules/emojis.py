7d153f66545ba08912fe1949ff2cd25e7cfd3866
Clarisse A Campos <tetearruda17@gmail.com>
2026-08-19 16:46:56 -0300

    none

---
+++ stack-based/python-nb/modules/emojis.py
@@ -0,0 +1,4 @@
+import emoji
+
+print(emoji.emojize("Python is :thumbs_up:"))
+print(emoji.emojize("I :red_heart:  coding!"))
--- stack-based/python-nb/modules/math.py
+++ stack-based/python-nb/modules/math.py
@@ -1,7 +1,7 @@
-from math import sqrt,ceil,floor
+from math import sqrt,ceil,floor,trunc
 
 num = 5
 
 raiz = sqrt(num)
 print('Square root of {} is {:.4f}'.format(num,raiz))
-print('Ceil: {} Floor {}'.format(ceil(raiz),floor(raiz)))
+print('Ceil: {} Floor: {} Trunc: {}'.format(ceil(raiz),floor(raiz), trunc(raiz)))
