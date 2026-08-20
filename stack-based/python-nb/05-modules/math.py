52022be0c07c25214490f49ecaed9d624353c430
Clarisse A Campos <tetearruda17@gmail.com>
2026-08-20 18:15:37 -0300

    notebook do modulo de math

--- stack-based/python-nb/05-modules/math.py
+++
@@ -1,7 +0,0 @@
-from math import sqrt,ceil,floor,trunc
-
-num = 5
-
-raiz = sqrt(num)
-print('Square root of {} is {:.4f}'.format(num,raiz))
-print('Ceil: {} Floor: {} Trunc: {}'.format(ceil(raiz),floor(raiz), trunc(raiz)))
